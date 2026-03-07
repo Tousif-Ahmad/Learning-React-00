d29164dbf6372f55131ac8ddbc5546fb913c999c
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2026-03-08 03:19:52 +0500

    jsx (javascript xml)

---
+++ JSX/src/App.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import './App.css'
+const App = () => {
+  // let name = "Sam";
+  // let age = 23;
+  // function sayMyName(){
+  //   return "My name is Ali"
+  // }
+  // let fruits = ["apple", "mango", "orange", "banana"];
+  // let alpha = <p>Hello how are {2 + 2}</p>
+
+  return (
+    <>
+      {/* <h1>Hello {name} how are you doing</h1>
+      <p>{sayMyName()}</p> */}
+      {/* <p>{age >= 18 ? "You can drive" : "You can't drive"}</p> */}
+
+      {/* <h1 style={{
+        backgroundColor : "teal",
+        fontSize : "3rem",
+        fontStyle : "italic"
+      }}>Hello How are you doing</h1> */}
+
+      {/* {fruits.map((items, idx)=>{
+          return <p key={idx}>{idx + 1}:  {items}</p>
+      })} */}
+
+      {/* {alpha} */}
+
+      {/* BEHIND THE SCENES  */}
+
+      {/* {React.createElement("h1", {}, "HELLO")} */}
+
+      {/* {React.createElement("h1", {className : "heading", style : {
+        backgroundColor : "red"
+      }}, "Hello how are you")} */}
+
+      
+      
+    </>
+  )
+}
+
+export default App
