8d3d250f5c08cbf6a1bd984e2903a5e4062d1eb3
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2026-03-14 08:24:49 +0500

    Regular css

---
+++ Regular-CSS/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Card from "./Components/Card";
+const App = () => {
+  return (
+    <>
+      <div className="red">Hello World</div>
+      <button className="btn">click me</button>
+      <Card/>
+      <p className="faltu">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque sit ut asperiores unde nisi ab impedit eveniet est ea expedita?</p>
+    </>
+  );
+};
+
+export default App;
---
+++ Regular-CSS/src/Components/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import style from '../Components/Card.module.css'
+const Card = () => {
+  return (
+   <>
+        <div className='btn'>Card</div>
+        <p className={style.faltu}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores fugiat voluptate libero adipisci quis reprehenderit magni facilis alias fuga aspernatur!</p>
+   </>
+    
+  )
+}
+
+export default Card
